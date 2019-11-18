bb6d9fd89bf7c601ff0427856bd3087342a1f443
Richard Gibert <richard@gibert.ca>
2019-11-17 21:43:10 -0500

    switch to using meta dependency - remove var include, use meta when instead - add versions to molecule reqs - add systemd tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,7 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_bin_file(host):
     assert host.file(
-        "/usr/local/share/prometheus_node_exporter-0.18.1" +
+        "/usr/local/share/prometheus_node_exporter-0.18.1"
         "/node_exporter-0.18.1.linux-amd64/node_exporter"
     ).exists
 
@@ -17,3 +17,11 @@ def test_service_file(host):
     assert host.file(
         "/etc/systemd/system/prometheus_node_exporter.service"
     ).exists
+
+
+def test_service_enabled(host):
+    assert host.service('prometheus_node_exporter').is_enabled
+
+
+def test_service_running(host):
+    assert host.service('prometheus_node_exporter').is_running
